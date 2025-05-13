9ec270c311f49c63fc35a059210497d6b161d4b5
g4gamers1 <cappelle.kevin123@gmail.com>
2025-05-13 18:11:48 +0200

    completed bookbot

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-
-import stats  
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
-        file_content = f.read()
-        print(file_content)
-
-
-    
-
+import sys
+import stats 
 def main():
-    stats.structure_text()
+        book_path = get_path_to_book()
+        book_text = get_book_text(book_path)
+        num_words = stats.get_num_words(book_text)
+        get_num_characters = stats.count_chars(book_text)
+        sorted_dict = stats.sort_dict_by_value(get_num_characters)
+        structure_text(book_path, num_words, sorted_dict)
+        
+        
+    #stats.structure_text()
 
-main()
+def get_path_to_book():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    return sys.argv[1] 
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def structure_text(book_path, num_words, sorted_dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}..")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_dict:
+        if not item[0].isalpha():
+            continue
+        print(f"{item[0]}: {item[1]}")
+    print("============= END ===============")
+
+         
+main() 
--- stats.py
+++ stats.py
@@ -1,14 +1,7 @@
-def get_book_as_string():
-    num_words = 0
-    with open("books/frankenstein.txt") as f:
-        for line in f:
-            line.split()
-            for word in line.split():
-                num_words += 1
-        print(f"Found {num_words} total words")
+def get_num_words(book):
+    return len(book.split())
+    # Count the number of words in the book
     
-
-
 def count_chars(text):
     char_counts = {}
     for char in text:
@@ -21,31 +14,7 @@ def count_chars(text):
             char_counts[lower_char] = 1
     return char_counts
 
-def get_num_characters_each_time():
-    with open("books/frankenstein.txt") as f:
-        text = f.read()
-    return count_chars(text)
-
-def sort_dict_by_value():
-    d = get_num_characters_each_time()
-    # Sort the dictionary by value in descending order
+def sort_dict_by_value(d):
     sorted_counts = sorted(d.items(), key=lambda item: item[1], reverse=True)
     return(sorted_counts) 
-
-def sorted_dict_to_spaced_prints(sorted_dict):
-    for char, count in sorted_dict:
-        if char == "\n":
-            print("\\n:", count)
-        else:
-            print(f"{char}: {count}")
-    
-
-
-def structure_text():
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    get_book_as_string()
-    print("--------- Character Count -------")
-    sorted_dict_to_spaced_prints(sort_dict_by_value())
-    print("============= END ===============")
+    # Sort the dictionary by value in descending order
